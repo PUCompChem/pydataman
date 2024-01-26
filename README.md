e0a3f309980e0fcdd47719ab9027feef2ed14340
GerganaTancheva123 <gerganatancheva1@gmail.com>
2024-01-26 11:59:15 +0200

    clean README

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # pydataman
 Python utilities for data management
 
-Google colab
-[Open in Google Colab](https://colab.research.google.com/drive/1b7AOrQiGQYxI0pBtKqKPtco3zjIeQscR?usp=sharing)
--- ioutils/meterac/data_access.py
+++ ioutils/meterac/data_access.py
@@ -7,7 +7,7 @@ def read_metadata(request_url):
     response_nodes = requests.get(request_url)
 
     if response_nodes.status_code != 200:
-        print("Status code ",response_nodes.status_code)
+        print("Status code ", response_nodes.status_code)
     data_nodes = response_nodes.content.decode('utf-8')
 
     meta_data = list(csv.reader(data_nodes.splitlines(), delimiter=','))
